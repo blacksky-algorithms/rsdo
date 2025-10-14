a8d09f2d70252cf2d239de20cf8f7f6b04b3ca65
Clinton Bowen <clinton.bowen@gmail.com>
2025-10-13 18:44:46 -0700

    update to fix sbom issue

--- .github/scripts/generate_sbom.sh
+++ .github/scripts/generate_sbom.sh
@@ -3,29 +3,25 @@ set -euxo pipefail
 
 # Function to generate SBOM files
 generate_sbom() {
-    local VERSION="$1"
-    local FORMAT="$2"
-    
-    cargo cyclonedx --format "$FORMAT" --override-filename "rsdo-sbom-${VERSION}"
+    local FORMAT="$1"
+
+    cargo cyclonedx --format "$FORMAT" --override-filename "rsdo-sbom"
 }
 
 # Function to generate both JSON and XML SBOM files
 generate_all_sbom() {
-    local VERSION="$1"
-    
-    generate_sbom "$VERSION" "json"
-    generate_sbom "$VERSION" "xml"
+    generate_sbom "json"
+    generate_sbom "xml"
 }
 
 # Main function
 main() {
-    local VERSION="$1"
-    local FORMAT="${2:-all}"
-    
+    local FORMAT="${1:-all}"
+
     if [ "$FORMAT" = "all" ]; then
-        generate_all_sbom "$VERSION"
+        generate_all_sbom
     else
-        generate_sbom "$VERSION" "$FORMAT"
+        generate_sbom "$FORMAT"
     fi
 }
 
